02fe610609486e4cd994be1eb3debd8bc39c2b1e
Paula Kordes <81625864+paulalkh@users.noreply.github.com>
2023-12-06 20:37:09 +0100

    Update entity_extraction.py

--- entity_extraction.py
+++ entity_extraction.py
@@ -1,23 +1,19 @@
+pip install transformers
+pip install spacy
 from transformers import pipeline
 import spacy
 
-# Load the llama model
 llama_model = pipeline("text-generation", model="Llama/llama-base-0001", tokenizer="Llama/llama-base-0001")
 
-# Load the spaCy model for entity extraction
 nlp = spacy.load("en_core_web_sm")
 
 def get_llama_output(question):
-    # Generate text using the llama model
     llama_output = llama_model(question, max_length=200, num_return_sequences=1)
-    
-    # Extract the raw text from the output
     raw_text = llama_output[0]['generated_text']
     
     return raw_text
 
 def extract_entities(text):
-    # Use spaCy for entity extraction
     doc = nlp(text)
     entities = []
     
